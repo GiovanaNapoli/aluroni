adb68f6269c6d9e1b3d6cdca346b45cf0c23026d
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2023-08-27 22:18:16 -0300

    feat: create search component

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Buscador.module.scss'
+import {CgSearch} from 'react-icons/cg'
+
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+export default function Buscador ({search, setSearch}: Props) {
+  return (
+  <div className={styles.buscador}>
+    <input value={search} onChange={(event) => setSearch(event.target.value)} />
+    <CgSearch size={20} color="#4c4d5e" />
+  </div>)
+}
