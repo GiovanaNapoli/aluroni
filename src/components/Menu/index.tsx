d657da996e166497bd3072630adb9393778d130e
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2023-10-23 23:08:30 -0300

    feat: add menu component to route file

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,25 @@
 import styles from './Menu.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 export default function Menu () {
+
+  const rotas = [{
+    label: 'Ínicio',
+    to: '/'
+  }, {
+    label: 'Cardápio',
+    to: '/Cardapio'
+  }, {label: 'Sobre', to: '/Sobre'}];
+
   return (
     <nav className={styles.menu}>
       <Logo />
+      <ul>
+        {rotas.map((rota, index) => (
+          <li key={index} className={styles.menu__link}>
+            <a className={styles.menu__list} href={rota.to}>{rota.label}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import routes from './routes';
+import Menu from 'components/Menu';
 
 export default function AppRoutes(){
   return(
     <Router>
+      <Menu />
       <Routes>
         {routes.map((route) => (
           <Route key={route.id} path={route.path} element={route.element} />
