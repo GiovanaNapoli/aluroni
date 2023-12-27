2b61a55afe9550f7dabb37eaba36f78775f03671
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2023-12-27 18:11:24 -0300

    refactor: create a new component to show tags of dishes

---
+++ src/components/TagsPrato/index.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames';
+import styles from './TagsPrato.module.scss';
+import { Prato } from 'types/prato';
+
+export default function TagsPrato ({category, size, serving, price}: Prato) {
+  return (
+    <div className={styles.tags}>
+      <div className={classNames({
+        [styles.tags__tipo]: true,
+        [styles[`tags__tipo__${category.label.toLowerCase()}`]]: true
+      })}>{category.label}</div>
+      <div className={styles.tags__porcao}>{size}</div>
+      <div className={styles.tags__qtdpessoas}>Serve {serving} pessoa</div>
+      <div className={styles.tags__valor}>R${price.toFixed(2)}</div>
+    </div>
+  );
+}
