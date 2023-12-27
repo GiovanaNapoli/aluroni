0b9073db679b2803f58fb4745fbc626cc5c928af
Giovana Aparecida Napoli da Silva <32879408+GiovanaNapoli@users.noreply.github.com>
2023-12-27 18:12:24 -0300

    refactor: create a files to export all components and pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Cardapio} from './Cardapio';
+export { default as Inicio } from './Inicio';
+export { default as NotFound } from './NotFound';
+export { default as PaginaPadrao } from './PaginaPadrao';
+export { default as Prato } from './Prato';
+export { default as Sobre } from './Sobre';
